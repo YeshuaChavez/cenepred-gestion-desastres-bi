3b8cdb41c34a48ffc4750d67b6b40c474e6c2745
YeshuaChavez <yeshua.chavez@unmsm.edu.pe>
2026-08-13 19:56:22 -0500

    feat(ui): update logo asset to local SVG file /images/logo_cenepred.svg and add favicon tag in index.html

--- apps/webapp/src/data/mockData.ts
+++ apps/webapp/src/data/mockData.ts
@@ -1,7 +1,7 @@
 import { RegionData, MefDepartment, PliegoEjecutor, NationalMeta } from '../types';
 import realData from './realData.json';
 
-export const LOGO_CENEPRED = "https://lh3.googleusercontent.com/aida-public/AB6AXuDVmjmpjymZJ7JDI7MSb12X3KHSUHwg4tvVHBLw8D5EBio7RAakaXl2sTKfwatTWyZ_t84mdN2mbixH91NAVKoVVGKvzl7bkxQBqAwcGi77pUZQ4gmoQV_C80RZYoWjydnS2F6eW7gb6qUGUuNupE5gdwLsro9w2-GYbYvX_M-b5QRHa3HtX09GxorFrGuUu_zziZm-G6jx5FczvkUbMfz9nTPtwe2vm7piPrkP59EfihE4Ia2W-T_Ang";
+export const LOGO_CENEPRED = "/images/logo_cenepred.svg";
 export const PROFILE_AVATAR = "https://lh3.googleusercontent.com/aida-public/AB6AXuD8jz68D6Jdn2vrp2B1EXhtnnZetQZ-L18u9Z-kSluvQ4urv9dJ4x6m-21X1df2W8MLcQDAzH4Jy3v96OXlIVXUpDIuJMwoJD72UuQ9OXxgsvOR6zXTH-YuXlLDTKtulsA5__uk6ow2_PTbxPk-Z3EJ_G7rc4F-VyV0XxQ2T9yBuCmOhA7OtLcMZOJmqmCh-lTXg1cHt2wqAVvp60TxJbXeTXNzM7UjTPix1BrE_Ob8TCnjkolocvJnIA";
 
 export const NATIONAL_META: NationalMeta = realData.meta;
